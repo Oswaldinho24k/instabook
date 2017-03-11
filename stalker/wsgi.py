d3f6b050072d4502ee6694674e3a39d1c50f3625
Oswaldo <oswalfut_96@hotmail.com>
2017-03-11 13:31:40 -0600

    daleeeee

--- stalker/wsgi.py
+++ stalker/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "stalker.settings.pro")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "stalker.settings")
 
 application = get_wsgi_application()
