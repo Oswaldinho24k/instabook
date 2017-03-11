0809bbb619fa647936b87dfda8502ce38f987750
Oswaldo <oswalfut_96@hotmail.com>
2017-03-11 13:45:56 -0600

    daleeeeeeeeeeee

--- stalker/settings/base.py
+++ stalker/settings/base.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(os.path.join(__file__
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'z%e$^xzrkduay-zcnywn!6ezwh%r6g8sq=b8s@01)94)d27hi%'
+
 SECRET_KEY = MYKEY
 
 
--- stalker/wsgi.py
+++ stalker/wsgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "stalker.settings")
+#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "stalker.settings")
+os.environ['DJANGO_SETTINGS_MODULE'] = 'stalker.settings.pro'
 
 application = get_wsgi_application()
